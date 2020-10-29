7bcdf7d17a1abc20f458245b0cce59d8f1bf610e
reygutierrez11 <reygutierrez11@users.noreply.github.com>
2020-10-28 21:34:14 -0500

    final push hopefully, cleaned up spacing

--- javaScript/doThings.js
+++ javaScript/doThings.js
@@ -2,6 +2,5 @@ function myFunction()
 {
   document.getElementById("button").innerHTML = "Thank you for joining the community!";
 }
-
 var d = new Date();
 document.getElementById("date").innerHTML = d;
